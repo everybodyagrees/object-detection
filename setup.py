60d3d3c0baca9a3e7b5149042094c2d5091c733d
everybodyagrees <meera1989parmar@gmail.com>
2024-11-12 14:19:40 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ObjectDetection',
+    version= '0.0.0',
+    author= 'Nirma Mtech CS',
+    packages= find_packages(),
+    install_requires = []
+
+)
